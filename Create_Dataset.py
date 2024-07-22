52b48d62799e34a59349df32d051e84ec6bda5a6
Prakshenjay Bhati <prakshenjay@gmail.com>
2024-07-22 13:49:03 +0530

    Update Create_Dataset.py

--- Create_Dataset.py
+++ Create_Dataset.py
@@ -43,6 +43,6 @@ for dir_ in os.listdir(DATA_DIR):
             data.append(data_aux)
             labels.append(dir_)
 
-f = open('data.pickle', 'wb')
+f = open('Data/data.pickle', 'wb')
 pickle.dump({'data': data, 'labels': labels}, f)
 f.close()
